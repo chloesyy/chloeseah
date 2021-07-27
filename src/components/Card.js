ebc1d3a9dba2a257a277d21008ddc58786c10f2a
chloesyy <chlyiyi@hotmail.com>
2021-07-27 22:40:40 +0800

    add intro animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,144 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useTransition, animated } from 'react-spring';
+import { useSpring, animated, useChain, useSpringRef } from 'react-spring';
 import './Card.css';
 
 function Card() {
-    const [cardVisible, setCardVisible] = useState(false);
-    const transitionCard = useTransition(cardVisible, {
+    const cardRef = useSpringRef();
+    const cardAnimation = useSpring({
         from: {
             opacity: 0
         },
-        enter: {
+        to: {
             opacity: 1
         },
         config: {
-            friction: 100,
-        }
-    });
-    window.onload = () => {
-        setCardVisible(true);
-    }
+            friction: 100
+        },
+        ref: cardRef
+    })
+
+    const contentRef = useSpringRef();
+    const contentAnimation = useSpring({
+        from: {
+            opacity: 0,
+            y: 50
+        },
+        to: {
+            opacity: 1,
+            y: 0
+        },
+        config: {
+            duration: 500
+        },
+        ref: contentRef
+    })
+
+    const headingRef = useSpringRef();
+    const headingAnimation = useSpring({
+        from: {
+            opacity: 0,
+            y: 50
+        },
+        to: {
+            opacity: 1,
+            y: 0
+        },
+        config: {
+            duration: 500
+        },
+        ref: headingRef
+    })
+
+    const smallTextRef = useSpringRef();
+    const smallTextAnimation = useSpring({
+        from: {
+            opacity: 0,
+            y: 50
+        },
+        to: {
+            opacity: 1,
+            y: 0
+        },
+        config: {
+            duration: 500
+        },
+        ref: smallTextRef
+    })
+
+    const AnimatedLink = animated(Link)
+    const linkedinRef = useSpringRef();
+    const linkedinAnimation = useSpring({
+        from: {
+            opacity: 0,
+            scale: 1
+        },
+        to: [
+            {opacity: 1, scale: 1.5},
+            {opacity: 1, scale: 1}
+        ],
+        config: {
+            duration: 250
+        },
+        ref: linkedinRef
+    })
+
+    const instagramRef = useSpringRef();
+    const instagramAnimation = useSpring({
+        from: {
+            opacity: 0,
+            scale: 1
+        },
+        to: [
+            {opacity: 1, scale: 1.5},
+            {opacity: 1, scale: 1}
+        ],
+        config: {
+            duration: 250
+        },
+        ref: instagramRef
+    })
+
+    const githubRef = useSpringRef();
+    const githubAnimation = useSpring({
+        from: {
+            opacity: 0,
+            scale: 1
+        },
+        to: [
+            {opacity: 1, scale: 1.5},
+            {opacity: 1, scale: 1}
+        ],
+        config: {
+            duration: 250
+        },
+        ref: githubRef
+    })
+
+    useChain([cardRef, contentRef, headingRef, smallTextRef, linkedinRef, instagramRef, githubRef], [0, 0.1, 0.2, 0.3, 0.6, 0.7, 0.8], 4000);
+
     return (
         <>
-            {transitionCard((style, item) => item && 
-            <animated.div style={style} className='card'>
+            <animated.div style={cardAnimation} className='card'>
                 <div className='card-intro'>
-                    <p className='content'>Hey! I'm</p>
-                    <h1>chloeseah.</h1>
-                    <p className='small-text'>Welcome to my space on the internet.</p>
+                    <animated.p style={contentAnimation} className='content'>Hey! I'm</animated.p>
+                    <animated.h1 style={headingAnimation} className='heading'>chloeseah.</animated.h1>
+                    <animated.p style={smallTextAnimation} className='small-text'>Welcome to my space on the internet.</animated.p>
                 </div>
                 <div className='social-icons-container'>
                     <div className='social-icons'>
-                        <Link className='social-icon-link linkedin' to={{ pathname: 'https://www.linkedin.com/in/chloe-seah-1456901b0/' }} target='_blank' aria-label='LinkedIn'>
+                        <AnimatedLink style={linkedinAnimation} className='social-icon-link linkedin' to={{ pathname: 'https://www.linkedin.com/in/chloe-seah-1456901b0/' }} target='_blank' aria-label='LinkedIn'>
                             <i className='fab fa-linkedin'></i>
-                        </Link>
-                        <Link className='social-icon-link instagram' to={{ pathname: 'https://www.instagram.com/chloe_syy_/' }} target='_blank' aria-label='Instagram'>
+                        </AnimatedLink>
+                        <AnimatedLink style={instagramAnimation} className='social-icon-link instagram' to={{ pathname: 'https://www.instagram.com/chloe_syy_/' }} target='_blank' aria-label='Instagram'>
                             <i className='fab fa-instagram'></i>
-                        </Link>
-                        <Link className='social-icon-link github' to={{ pathname: 'https://github.com/chloesyy' }} target='_blank' aria-label='Github'>
+                        </AnimatedLink>
+                        <AnimatedLink style={githubAnimation} className='social-icon-link github' to={{ pathname: 'https://github.com/chloesyy' }} target='_blank' aria-label='Github'>
                             <i className='fab fa-github'></i>
-                        </Link>
+                        </AnimatedLink>
                     </div>
                 </div>
-            </animated.div>)}
+            </animated.div>
         </>
     )
 }
